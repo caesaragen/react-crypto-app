dcbab791901297fae193792e1c513e11ec84aac8
caesaragen <caesaragen@gmail.com>
2022-01-20 22:50:18 +0300

    add reducer

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,5 @@
+const CryptoDetails = () => {
+  return <h1>CryptoDetails</h1>;
+};
+
+export default CryptoDetails;
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,7 @@
+const Cryptocurrencies = () => {
+    return <div>
+      cryptocurrencies
+  </div>;
+};
+
+export default Cryptocurrencies;
