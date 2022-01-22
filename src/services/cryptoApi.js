9f4afc82bda07d2cd3e3e32ba576fc27a76a9654
caesaragen <caesaragen@gmail.com>
2022-01-22 13:04:12 +0300

    fetch crypto, add filter

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,18 +1,18 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-    "x-rapidapi-key": "175543cd42msh8185be4d5aff10bp16d682jsn37db33d9d376",
 };
-const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
-const createRequest = (url) => ({url, headers:cryptoApiHeaders})
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    basQuery: fetchBaseQuery({ baseUrl }),
+    reducerPath: "cryptoApi",
+    baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query:() => createRequest('/exchanges')
-        })
-    })
-})
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+    }),
+});
+export const { useGetCryptosQuery } = cryptoApi;
