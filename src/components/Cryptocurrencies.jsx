1d88fae46a0590eee94a08478d96a3d6daabb738
Joel Aduma <caesaragen@gmail.com>
2022-03-16 13:16:53 +0300

    add linter

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -15,7 +15,7 @@ const Cryptocurrencies = ({ simplified }) => {
 
   useEffect(() => {
 
-    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
     setCrypto(filteredData)
 
   }, [cryptosList, searchTerm]);
@@ -37,7 +37,7 @@ const Cryptocurrencies = ({ simplified }) => {
             <Link to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
-                extra={<img className='crypto-image' src={currency.iconUrl} />}
+                extra={<img className='crypto-image' src={currency.iconUrl} alt="" />}
                 hoverable
               >
                 <p>Price:{millify(currency.price)}</p>
